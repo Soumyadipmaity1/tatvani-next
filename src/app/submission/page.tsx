f59f11001b3bf2d5bf6e7ef340cf78309f1dd2df
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-28 02:41:43 +0530

    Add SubmissionPage component with SubmissionForm

---
+++ src/app/submission/page.tsx
@@ -0,0 +1,12 @@
+import SubmissionForm from '@/components/SubmissionPage/SubmissionForm'
+import React from 'react'
+
+function SubmissionPage() {
+  return (
+    <div suppressHydrationWarning={true}>
+      <SubmissionForm/>
+    </div>
+  )
+}
+
+export default SubmissionPage
