40f607807ae8878a2e4feca65e4e9fd4dbe5a992
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-28 10:28:52 +0530

    Add AddAdvertisePage component for admin dashboard

---
+++ src/app/(admin)/add-advertise/page.tsx
@@ -0,0 +1,18 @@
+import AddAdvertise from "@/components/AdminPage/addAdvertise";
+import Header from "@/components/Menubars/Header";
+import Sidebar from "@/components/Menubars/Sidebar";
+
+const AddAdvertisePage = () => {
+    return (
+      <div className="admin-dashboard flex" suppressHydrationWarning={true}>
+        <Sidebar />
+        <div className="flex-1 p-8 bg-gray-800 pt-16  pl-64">
+          <Header />
+          <AddAdvertise />
+        </div>
+      </div>
+    );
+  };
+  
+  export default AddAdvertisePage;
+  
