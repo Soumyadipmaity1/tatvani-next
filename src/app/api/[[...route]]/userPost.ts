dfedaf20391355beb00e864fb06bf8914c819c74
Abhik Patra <theabhik2020@gmail.com>
2024-12-30 00:14:01 +0530

    Add CRUD operations for user posts in userPost API

--- src/app/api/[[...route]]/userPost.ts
+++ src/app/api/[[...route]]/userPost.ts
@@ -8,7 +8,7 @@ export const userPost = new Hono()
 
             const { firstName, lastName, college, email, mobileNo, category, title, content } = await c.req.json();
 
-            console.log("firstName"+firstName, "lastName"+lastName, "college"+college, "email"+email, "mobileNo"+mobileNo, "category"+category, "title"+title, "content"+content)
+            // console.log("firstName"+firstName, "lastName"+lastName, "college"+college, "email"+email, "mobileNo"+mobileNo, "category"+category, "title"+title, "content"+content)
 
 
 
@@ -27,8 +27,54 @@ export const userPost = new Hono()
             })
             return c.json({ message: "Post added successfully" }, 201);
 
-        } catch (error:any) {
+        } catch (error: any) {
             console.log(error)
             throw new HTTPException(500, { message: "Internal Server Error" });
         }
-    })
+    }).get('/get-posts', async (c) => {
+        try {
+            const posts = await db.userPost.findMany();
+            return c.json(posts, 200);
+        } catch (error) {
+            throw new HTTPException(500, { message: "Internal Server Error" });
+        }
+    }).get('/get-post/:id', async (c) => {
+        try {
+            const id = c.req.param("id");
+            const post = await db.userPost.findUnique({
+                where: {
+                    id: id
+                }
+            });
+            return c.json(post, 200);
+        } catch (error) {
+            throw new HTTPException(500, { message: "Internal Server Error" });
+        }
+    }).get("/approve-post/:id", async (c) => {
+        try {
+            const id = c.req.param("id");
+            await db.userPost.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    approved: true
+                }
+            });
+            return c.json({ message: "Post approved" }, 200);
+        } catch (error) {
+            throw new HTTPException(500, { message: "Internal Server Error" });
+        }
+    }).delete("/delete-post/:id", async (c) => {
+        try {
+            const id = c.req.param("id");
+            await db.userPost.delete({
+                where: {
+                    id: id
+                }
+            });
+            return c.json({ message: "Post deleted" }, 200);
+        } catch (error) {
+            throw new HTTPException(500, { message: "Internal Server Error" });
+        }
+    });
