d383668de06b4eee1ca0d6156431f7391bce6a03
Abhik Patra <theabhik2020@gmail.com>
2024-12-30 01:16:00 +0530

    Add advertisement route to API for handling advertisement requests

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,12 +3,13 @@ import { handle } from 'hono/vercel'
 import { users } from './users'
 import { post } from './post'
 import { userPost } from './userPost'
+import { advertisement } from './advertisement'
 
 // export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.route('/user', users).route('/post', post).route("/userPost",userPost)
+app.route('/user', users).route('/post', post).route("/userPost", userPost).route("/advertise", advertisement)
 
 export const GET = handle(app)
 export const POST = handle(app)
