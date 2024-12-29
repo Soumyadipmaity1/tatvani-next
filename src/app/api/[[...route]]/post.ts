f4fa19cdb0fc7d8c85dd635e68ce309a0abe5ec1
Abhik Patra <theabhik2020@gmail.com>
2024-12-29 20:28:07 +0530

    Add validation schema and error handling for adding posts in API

--- src/app/api/[[...route]]/post.ts
+++ src/app/api/[[...route]]/post.ts
@@ -1,11 +1,15 @@
+import { addPostSchema } from "@/schema/post.schema";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { HTTPException } from 'hono/http-exception';
 
 
 export const users = new Hono()
-.post('/add-post',async (c)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-})
+    .post('/add-post', zValidator("json", addPostSchema), async (c) => {
+        try {
+            const { title, author, content, category, image_url } = c.req.valid("json");
+            
+        } catch (error) {
+            throw new HTTPException(500, { message: 'An error occurred' })
+        }
+    })
---
+++ src/schema/post.schema.ts
@@ -0,0 +1,11 @@
+import { Category } from "@prisma/client";
+import { z } from "zod";
+
+
+export const addPostSchema = z.object({
+    title: z.string().min(5, { message: "Title must be at least 5 characters." }).max(50, { message: "Title must be at most 50 characters." }),
+    author: z.string().min(3, { message: "Author name must be at least 3 characters." }).max(15, { message: "Author name must be at most 15 characters." }),
+    content: z.string().min(10, { message: "Content must be at least 10 characters." }),
+    category: z.enum(['Article', 'Poetry', 'Stories'], { message: "Category must be one of 'article', 'poetry', or 'stories'." }),
+    image_url: z.string(),
+})  
