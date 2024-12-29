c845c0a9dfe3de08930cfa8bd1d5c310ec2d2ffc
Abhik Patra <theabhik2020@gmail.com>
2024-12-29 20:05:37 +0530

    Implement authentication check in AuthRootLayout to redirect authenticated users to the dashboard

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import "../globals.css";
 import QueryProvider from '@/context/QueryProvider';
 import { Toaster } from 'react-hot-toast';
+import isAuthnticated from '@/hooks/user/isAuthnticated';
+import { redirect } from 'next/navigation';
 
 
 
@@ -15,11 +17,19 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-function AuthRootLayout({ children }: { children: React.ReactNode }) {
+async function AuthRootLayout({ children }: { children: React.ReactNode }) {
+
+
+  const auth = await isAuthnticated();
+
+  if (auth) {
+    redirect("/dashboard");
+  }
+
   return (
     <html lang="en" >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `} 
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
 
         <QueryProvider>{children} </QueryProvider>
