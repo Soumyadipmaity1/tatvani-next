b9be0c8772048a2d5523485e733d6d2b573983d7
Abhik Patra <theabhik2020@gmail.com>
2024-12-29 19:50:39 +0530

    Add QueryProvider component and restructure layout for authentication routes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Menubars/Footer";
 import Navbar from "@/components/Menubars/Navbar";
 import { usePathname } from "next/navigation";
 import toast, { Toaster } from 'react-hot-toast'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -35,19 +35,19 @@ export default function RootLayout({
   const pathname = usePathname();
   const noLayoutRoutes = ["/dashboard", "/add-post", "/review-posts"];
   const showLayout = pathname ? !noLayoutRoutes.includes(pathname) : false;
-  const queryClient = new QueryClient()
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `} suppressHydrationWarning
       >
-        <QueryClientProvider client={queryClient}>
+
 
           {showLayout && <Navbar />}
           {children}
           {showLayout && <Footer />}
           <Toaster />
-        </QueryClientProvider>
+    
       </body>
     </html>
   );
