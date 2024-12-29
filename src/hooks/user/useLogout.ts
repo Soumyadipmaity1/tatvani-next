207a91f0e7b660e9effb2f353817839672afaa5d
Abhik Patra <theabhik2020@gmail.com>
2024-12-29 20:11:03 +0530

    Add custom hook for user logout with success and error handling

---
+++ src/hooks/user/useLogout.ts
@@ -0,0 +1,23 @@
+"use client"
+
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+export default function useLogin() {
+    const router = useRouter();
+    const mutation = useMutation({
+        mutationFn: async (data: any) => {
+            const res = await axios.get("/api/user/logout");
+            return res;
+        },
+        onSuccess: () => {
+            toast.success("Logout successful");
+            router.push("/login");
+        },
+        onError: (err: any) => {
+            toast.error(err.response.data.message || "Login failed");
+        }
+    });
+    return mutation;
+}
