3fdbaf8d7ed7223fbd9f54062505a129aabb93dc
Abhik Patra <theabhik2020@gmail.com>
2024-12-29 23:24:29 +0530

    Add timestamps and approval status to UserPost model; update API to include category in post creation

--- src/app/api/[[...route]]/userPost.ts
+++ src/app/api/[[...route]]/userPost.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 
@@ -5,8 +6,23 @@ export const post = new Hono()
     .post('/add-post', async (c) => {
         try {
 
-            const { firstName, lastName, college, email, mobileNo, type, title, content } = await c.req.parseBody();
-            
+            const { firstName, lastName, college, email, mobileNo, category, title, content } = await c.req.json();
+
+
+            await db.userPost.create({
+                data: {
+                    firstName,
+                    lastName,
+                    college,
+                    email,
+                    mobileNo,
+                    category,
+                    title,
+                    content
+                }
+            })
+            return c.json({ message: "Post added successfully" }, 201);
+
         } catch (error) {
             throw new HTTPException(500, { message: "Internal Server Error" });
         }
