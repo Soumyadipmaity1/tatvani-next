47b2fa57c2d95d4a0ed0b5f6138ecbf112ff497b
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-31 18:15:27 +0530

    Enable dark mode support in Tailwind CSS configuration and extend color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
-
 export default {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +8,13 @@ export default {
   ],
   theme: {
     extend: {
-      darkMode: "class",
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        pitch: "#fec784",
+        darkBlue: "#2f296a",
+        darkGreen: "#1c7636",
+        bgDark: "#111827",
       },
     },
   },
