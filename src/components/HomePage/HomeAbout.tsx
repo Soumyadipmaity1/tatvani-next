35751f46de0c1361fa8aa5610d247e38eb8a58a5
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-31 18:16:04 +0530

    Refactor AboutSection styles for improved color consistency and enhanced readability

--- src/components/HomePage/HomeAbout.tsx
+++ src/components/HomePage/HomeAbout.tsx
@@ -4,13 +4,15 @@ import Image from 'next/image';
 
 const AboutSection: FC = () => {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-evenly px-6 py-20 bg-white space-y-8 lg:space-y-0 lg:space-x-16">
+    <div className="flex flex-col lg:flex-row items-center justify-evenly px-6 py-20 
+      bg-white dark:bg-[#111827] transition-colors duration-300 
+      space-y-8 lg:space-y-0 lg:space-x-16">
       <div className="text-left max-w-lg">
         <div>
-          <h1 className="text-4xl text-[#3d3b3a] font-bold mb-4">
+          <h1 className="text-4xl text-darkBlue dark:text-pitch font-bold mb-4">
             Driving Success, One Client At A Time
           </h1>
-          <p className="text-gray-700 mb-8">
+          <p className="text-gray-700 dark:text-gray-300 mb-8">
             Our commitment to excellence has led to remarkable achievements.
             Here are the values that reflect our dedication and the trust of
             our clients. We work tirelessly to ensure your success and
@@ -21,7 +23,12 @@ const AboutSection: FC = () => {
           </p>
           <Link
             href="/about"
-            className="bg-[#ff7f57] text-[#3d3b3a] font-bold px-6 py-3 mt-4 hover:bg-white hover:text-orange-600 border-2 shadow-lg border-orange-600 rounded"
+            className="bg-darkBlue text-white dark:text-darkBlue hover:text-darkBlue dark:bg-pitch 
+              font-bold px-6 py-3 mt-4 
+              hover:bg-pitch 
+              border-2 border-darkBlue dark:border-pitch
+             dark:hover:border-pitch  hover:bg-transparent dark:hover:bg-transparent dark:hover:text-pitch
+              rounded transition-all duration-300"
           >
             More About Us
           </Link>
@@ -32,7 +39,7 @@ const AboutSection: FC = () => {
         <Image
           src="https://via.placeholder.com/400x300"
           alt="Placeholder"
-          className="rounded-lg shadow-lg"
+          className="rounded-lg shadow-lg dark:shadow-blue-900/30"
           width={400}
           height={300}
         />
